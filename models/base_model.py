ae10998032050c82780347d03aac167ff5a8d39d
prisca2920 <prisca2920@gmail.com>
2023-07-13 01:41:54 -0700

    model

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""creates a basemodel class"""
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """ initiatilizes class basemodel"""
+    def __init__(self, id, *args, **kwargs):
+        """ the attributes """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            isoformat = "%Y-%m-%dT%H:%M:%S.%f"
+
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.strptime(value, isoformat))
+                elif key != '__class__':
+                    setattr(self, key, value)
+
+        storage.new(self)
+
+    def __str__(self):
+        """the string representation"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ returns a dict representation"""
+        dict = self.__dict__.copy()
+        dict["__class__"] = self.__class__.__name__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        return dict
