1d464b8551445dd04f69536402a3d630bf8fcb78
prisca2920 <prisca2920@gmail.com>
2023-07-14 18:50:41 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """user inherits from base"""
-from models import BaseModel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """initializes class user """
-    def __init__(self, *args, **kwargs):
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
-        super().__init__()
+    """defines public class attributes"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
